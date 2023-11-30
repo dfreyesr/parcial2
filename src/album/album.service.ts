9032ea68c6602ffe0b05c15c70be2d785200e6d3
Daniel Reyes <df.reyesr@uniandes.edu.co>
2023-11-30 10:33:30 -0500

    commit 2

---
+++ src/album/album.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  BusinessLogicException,
+  BusinessError,
+} from '../shared/business-errors';
+import { AlbumEntity } from './album.entity/album.entity';
+import { FotoEntity } from 'src/foto/foto.entity/foto.entity';
+
+@Injectable()
+export class AlbumService {
+  constructor(
+    @InjectRepository(AlbumEntity)
+    private albumRepository: Repository<AlbumEntity>,
+    @InjectRepository(FotoEntity)
+    private fotoRepository: Repository<FotoEntity>,
+  ) {}
+
+  async createAlbum(albumData: {
+    titulo: string;
+    fechaInicio: string;
+    fechaFin: string;
+  }): Promise<AlbumEntity> {
+    if (!albumData.titulo) {
+      throw new BusinessLogicException(
+        'El título no puede estar vacío',
+        BusinessError.BAD_REQUEST,
+      );
+    }
+
+    const newAlbum = this.albumRepository.create(albumData);
+    return await this.albumRepository.save(newAlbum);
+  }
+
+  async findAlbumById(id: number): Promise<AlbumEntity> {
+    const album = await this.albumRepository.findOne({
+      where: { id },
+      relations: ['fotos'],
+    });
+    if (!album) {
+      throw new BusinessLogicException(
+        `Álbum con ID ${id} no encontrado`,
+        BusinessError.NOT_FOUND,
+      );
+    }
+    return album;
+  }
+
+  async addPhotoToAlbum(
+    albumId: number,
+    photoId: number,
+  ): Promise<AlbumEntity> {
+    const id = photoId;
+    const album = await this.findAlbumById(albumId);
+    const photo = await this.fotoRepository.findOne({
+      where: { id },
+    });
+
+    if (!photo) {
+      throw new BusinessLogicException(
+        `Foto con ID ${photoId} no encontrada`,
+        BusinessError.NOT_FOUND,
+      );
+    }
+
+    const photoDate = new Date(photo.fecha);
+    if (
+      photoDate < new Date(album.fechaInicio) ||
+      photoDate > new Date(album.fechaFin)
+    ) {
+      throw new BusinessLogicException(
+        'La fecha de la foto no está dentro del rango del álbum',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    }
+
+    album.fotos = [...album.fotos, photo];
+    return await this.albumRepository.save(album);
+  }
+
+  async deleteAlbum(id: number): Promise<void> {
+    const album = await this.findAlbumById(id);
+
+    if (album.fotos.length > 0) {
+      throw new BusinessLogicException(
+        'No se puede eliminar un álbum con fotos asignadas',
+        BusinessError.PRECONDITION_FAILED,
+      );
+    }
+
+    await this.albumRepository.remove(album);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,31 @@ import { FotoModule } from './foto/foto.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AlbumModule } from './album/album.module';
 import { RedSocialModule } from './red-social/red-social.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FotoEntity } from './foto/foto.entity/foto.entity';
+import { AlbumEntity } from './album/album.entity/album.entity';
+import { RedSocialEntity } from './red-social/red-social.entity/red-social.entity';
+import { UsuarioEntity } from './usuario/usuario.entity/usuario.entity';
 
 @Module({
-  imports: [FotoModule, UsuarioModule, RedSocialModule, AlbumModule],
+  imports: [
+    FotoModule,
+    UsuarioModule,
+    AlbumModule,
+    RedSocialModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'museum',
+      entities: [FotoEntity, AlbumEntity, RedSocialEntity, UsuarioEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
